85b0154849a2a395af14ffdc0f53d1001377dbb7
Jean-Luc LYS <jeanluc.lys@gmail.com>
2022-06-02 21:14:33 +0200

    default spring project

---
+++ src/main/java/fr/esgi/project_manager_api/ProjectManagerApiApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.project_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerApiApplication.class, args);
+    }
+
+}
