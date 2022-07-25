8290f8acf054260adb4786f4c4a05362970bf2fb
Jean-Luc LYS <jeanluc.lys@gmail.com>
2022-07-25 15:45:06 +0200

    first test commented

--- src/main/java/fr/esgi/project_manager_api/ProjectManagerApiApplication.java
+++ src/main/java/fr/esgi/project_manager_api/ProjectManagerApiApplication.java
@@ -3,16 +3,26 @@ package fr.esgi.project_manager_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 @EntityScan("fr.esgi.project_manager_api.models")
-@EnableJpaRepositories("fr.esgi.project_manager_api.repository")
+@EnableJpaRepositories(basePackages="fr.esgi.project_manager_api.repository")
 public class ProjectManagerApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagerApiApplication.class, args);
     }
 
+
+    /*
+    @Bean(name="entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        return sessionFactory;
+    }
+     */
 }
--- src/test/java/fr/esgi/project_manager_api/ProjectManagerApiApplicationTests.java
+++ src/test/java/fr/esgi/project_manager_api/ProjectManagerApiApplicationTests.java
@@ -1,13 +1,60 @@
 package fr.esgi.project_manager_api;
 
-import org.junit.jupiter.api.Test;
+import fr.esgi.project_manager_api.controllers.UserController;
+import fr.esgi.project_manager_api.models.User;
+import fr.esgi.project_manager_api.services.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class ProjectManagerApiApplicationTests {
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.BDDMockito.given;
+
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class ProjectManagerApiApplicationTests {
+
+    /*
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private UserService userService;
+
 
     @Test
-    void contextLoads() {
+    public void shouldFindAllUser() throws Exception {
+        User user1 = new User();
+        user1.setLastname("gouro");
+        user1.setFirstname("teo");
+        user1.setEmail("teogouro@gmail.com");
+        user1.setPassword("1234");
+
+        given(this.userService.readAll())
+                .willReturn(asList(
+                        user1
+                )
+        );
+
+
+        this.mvc.perform(get("/all"))
+                .andExpect(status().isOk());
+        //.andExpect(jsonPath("$.length()", is(1)))
+        //.andExpect(jsonPath("$.[0].email", is("teogouro@gmail.com")));
     }
+    */
 
 }
